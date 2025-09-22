4706a5641cf78c37a5a127cfb03c4e95878f1f7c
Dhanya vangalapudi <dhanyavangalapudi@Dhanyas-MacBook-Air.local>
2025-09-23 00:47:56 +0530

    updated version

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(async ({ mode }) => ({
   // ensure correct base for GitHub Pages when building for production
   base: mode === "production" ? "/sowjanya-portofolio/" : "/",
   plugins: [
